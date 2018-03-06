562dfb6c8a81888cc185d04801f800d7d9c4e015
Layla Flight <layla.flight@greenschoolbrooklyn.com>
2018-03-06 23:28:26 +0000

    add

--- js/script.js
+++ js/script.js
@@ -3,7 +3,22 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 $(document).ready(function(){
-  
+ $.ajax({
+      url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC",
+      method: "GET",
+      success: function(response) {
+                     console.log(response);
+                console.log(response.data);
+                console.log(response.data[0]);
+               console.log(response.data[0].type);
+             console.log(response.data[0].title);
+             console.log(response.data[0].images.original.url);
+             $('body').html("<img src=" + response.data[4].images.original.url +"/>");
+      },    
+    });
+         
+         
+    
   
   
   
